ad34600419dbc81b111f653d349c799002a17c5c
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-12-05 01:43:47 -0800

    100-print_python_list_info.c inital commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists.
+ *
+ *  @p: function Parameter, PyObject list.
+ *
+ *
+ * return: void
+ *
+ *
+ **/
+
+void print_python_list_info(PyObject *p)
+{
+
+}
