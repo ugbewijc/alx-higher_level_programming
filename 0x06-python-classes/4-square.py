fb0f839daca34e2ce772ff6daa4db064e755b0f8
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-12-20 15:51:25 -0800

    4-square initail commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""A Square class"""
+
+class Square:
+    """Square class. size setter and getter, has area method"""
+    def __init__(self, size=0):
+        """Initialize Square data"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculate and returns area of square"""
+        return (self.__size * self.__size)
