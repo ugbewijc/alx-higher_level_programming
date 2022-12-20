b3f70c28b3a8befc75e855ba23946dd5307dbfd9
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-12-20 14:17:55 -0800

    0-square.py initial commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square:
+    '''
+    this is an empty Square class, just laying the foundation
+    '''
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+ALX-SE-9 Task on Python - Classes and Objects
+
+https://alx-intranet.hbtn.io/projects/247
