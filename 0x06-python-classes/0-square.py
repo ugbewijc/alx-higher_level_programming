04a70174f773cbd99b36d3b06e5f86079bdfcc74
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-12-20 14:35:21 -0800

    1-square initial commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 class Square:
-    '''
+    """
     this is an empty Square class, just laying the foundation
-    '''
+
+    Arg:
+
+    Return:
+
+    """
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class Square:
+
+        """
+        Square class, with a private attribute
+
+        Args:
+            size: size of the square
+
+        Returns: void
+
+        """
+
+    __size
+
+   def  __init__(self, size=0):
+       """Initializes the data."""
+       pass
