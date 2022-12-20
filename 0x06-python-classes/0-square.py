2ceb40685d608eed78d008d4e3337449ca60c73f
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-12-20 14:41:50 -0800

    Updated the comment section

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+    An Empty Sqare class
+"""
+
 class Square:
     """
     this is an empty Square class, just laying the foundation
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+    A sqaure class, with private class and initialization method
+"""
 
 class Square:
 
