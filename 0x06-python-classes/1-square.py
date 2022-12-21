a172acdd600fca5add11bc3e93ad6207ca8c51ed
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-12-21 01:40:46 -0800

    corrected indentation error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,5 +7,5 @@ class Square:
     """a Square Class"""
 
     def __init__(self, size):
-       """Initializes the data."""
-       self.__size = size
+        """Initializes the data."""
+        self.__size = size
